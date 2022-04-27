a124afcdfd9bae52b48708b5c9305d900f6a74d5
George Bacalu <b.george1401@gmail.com>
2022-04-27 20:22:13 +0300

    Initial commit

---
+++ src/main/java/com/project/SwaggerRestApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerRestApiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerRestApiDemoApplication.class, args);
+    }
+
+}
